26fd8c08e46777e90f4b313d5f7a7f9897b279a5
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-03-06 15:53:29 +0100

    Initial Commit

---
+++ GameOfLife/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GameOfLife
+//
+//  Created by Heiko Goes on 06.03.20.
+//  Copyright © 2020 Heiko Goes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
